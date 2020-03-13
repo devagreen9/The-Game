a2707fcafcaf58dd15c8cd60718f559e3ba7b70d
devagreen9 <61478502+devagreen9@users.noreply.github.com>
2020-03-13 11:51:48 -0400

    PsuedoCode

---
+++ Script.js
@@ -0,0 +1,13 @@
+Player 1 vs Computer both values equal 0;
+Player 1 starts by placing a circular peg in one of the grid spots;
+Computer makes a counter move;
+Check if there is four in a row;
+    four in a row vertically, horizontally, or diagonally;
+
+Continue in a loop;
+
+if player 1 has four in a row, then they win;
+if Computer has four in a row, then they win;
+
+
+
