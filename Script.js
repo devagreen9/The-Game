532d03c7920aab144591b942ac4397abb483b371
devagreen9 <61478502+devagreen9@users.noreply.github.com>
2020-03-17 12:45:34 -0400

    PsuedoCode & References

--- Script.js
+++ Script.js
@@ -10,6 +10,6 @@ if player 1 has four in a row, then they win;
 if player 2 has four in a row, then they win;
 
 References:
-https://www.youtube.com/watch?v=Z_IaJQojun8
-https://www.youtube.com/watch?v=vzxCC5ggyUc
+https://www.youtube.com/watch?v=Z_IaJQojun8 // what was the most helpful in the end
+https://www.youtube.com/watch?v=vzxCC5ggyUc // what  I initially tried to understand 
 
