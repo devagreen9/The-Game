6f6ff73107daaa29379fc9532289a94a06dac174
devagreen9 <61478502+devagreen9@users.noreply.github.com>
2020-03-17 08:49:02 -0400

    Add files via upload

--- Script.js
+++ Script.js
@@ -1,13 +1,13 @@
-Player 1 vs Computer both values equal 0;
+Player 1 vs Player 2 both values equal 0;
 Player 1 starts by placing a circular peg in one of the grid spots;
-Computer makes a counter move;
+Player 2 makes a counter move;
 Check if there is four in a row;
     four in a row vertically, horizontally, or diagonally;
 
 Continue in a loop;
 
 if player 1 has four in a row, then they win;
-if Computer has four in a row, then they win;
+if player 2 has four in a row, then they win;
 
 
 
